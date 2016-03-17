4870ce9a9442c53cbb7c65351425372bb51a05b0
Hawkgirl <hawkgirl@gmail.com>
2016-03-17 10:37:29 +0000

    removing commented code

--- openstack/check-zombie-resources.py
+++ openstack/check-zombie-resources.py
@@ -5,7 +5,6 @@ import sys
 import warnings
 warnings.filterwarnings("ignore")
 
-#cloud_obj =  get_ext_cloud("openstack",username='admin', password='zEPre25+UF_ba-wEp2e?', tenant_name='admin', auth_url='https://vcaas-prod.vcaas-ibm.com:5000/v2.0', cacert='/opt/stack/ssl/openstack.crt')
 cloud_obj =  get_ext_cloud("openstack")
 
 resources = cloud_obj.resources.list_zombie_resources()
@@ -16,9 +15,4 @@ if len(dic) is 0:
 	sys.exit(0)
 print dic
 sys.exit(1)
-'''
-print len(instances)
-for instance in instances:
-	print instance.id, instance.resource_type
-'''
 
