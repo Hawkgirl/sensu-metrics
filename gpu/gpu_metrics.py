747b84e5d9f78ab970ff702f33d709dac9930db7
root <Hawkgirl>
2017-09-21 08:17:58 +0000

    gpu meteric fixes

--- gpu/gpu_metrics.py
+++ gpu/gpu_metrics.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+from __future__ import division
 from pynvml import *
 
 import time
@@ -15,9 +16,10 @@ for i in range(deviceCount):
         handle = nvmlDeviceGetHandleByIndex(i)
         tasks = nvmlDeviceGetComputeRunningProcesses(handle)
         info = nvmlDeviceGetUtilizationRates(handle)
+        memory = nvmlDeviceGetMemoryInfo(handle)
 
-        print "%s.gpu%d.gpu_percent %d %d" % (hostname, i, info.gpu, now)
-        print "%s.gpu%d.gpumem_percent %d %d" % (hostname, i, info.memory, now)
-        print "%s.gpu%d.task_count %d %d" % (hostname, i, len(tasks), now)
+        print "%s.gpu.%d.gpu_percent %d %d" % (hostname, i, info.gpu, now)
+        print "%s.gpu.%d.gpumem_percent %f %d" % (hostname, i, memory.used/memory.total, now)
+        print "%s.gpu.%d.task_count %d %d" % (hostname, i, len(tasks), now)
 
 nvmlShutdown()
